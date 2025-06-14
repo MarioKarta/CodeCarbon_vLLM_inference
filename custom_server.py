26b91a7fd509f4f2500ce9c8778b0a6ecbccdffc
GB4_MARIO\mario <mario.kartawinata@gmail.com>
2025-06-14 10:34:14 +0200

    - added args to reduce memory usage

--- custom_server.py
+++ custom_server.py
@@ -27,6 +27,8 @@ def main():
     args.response_mode = "stream"
 
     args.trust_remote_code = True
+    args.enforce_eager = True
+    args.compilation_mode = "eager"
 
 
     asyncio.run(run_server(args))
